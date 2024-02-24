701251653af355e7b6c07c4ad03f427f9fff79b9
AndrewJaffray <130483685+AndrewJaffray@users.noreply.github.com>
2024-02-24 19:44:48 +0000

    added different user types and changed the homepage for each.

--- Website/models.py
+++ Website/models.py
@@ -11,7 +11,6 @@ class Note (db.Model):
     # if you want to get the columns from another table you use .whatever (user.email, user.password)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     first_name = db.Column(db.String(150), nullable=False)
