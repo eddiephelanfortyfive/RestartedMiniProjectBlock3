1044b42f0ae86ec7d4f93f4d6f0ee041a330f7b2
Patrickwalsh33 <patrickwalsh33@outlook.com>
2024-02-25 11:08:44 +0000

    profile page phone number field added

--- Website/views.py
+++ Website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Note
 from Website.models import Note
@@ -42,3 +42,18 @@ def delete_note():
 @login_required
 def profile():
     return render_template("profile.html", user=current_user)
+
+@views.route('/add-contact-number', methods=['POST'])
+def add_contact_number():
+    if request.method == 'POST':
+        contact_number = request.form.get('contactNumber')
+
+        # Validate and update contact number in the database
+        if contact_number:
+            current_user.contact_number = contact_number
+            db.session.commit()
+            flash('Contact number added successfully!', category='success')
+        else:
+            flash('Invalid contact number.', category='error')
+
+    return redirect(url_for('views.profile'))
